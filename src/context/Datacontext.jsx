3095f9c0f2858ff76efa1aaf33e99e7fbd0fe043
Ahmedalghdyry <ahmdalghdyry31@gmail.com>
2024-11-07 23:36:41 +0200

    Add files via upload

---
+++ src/context/Datacontext.jsx
@@ -0,0 +1,32 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { createContext, useReducer } from "react";
+ const ThemeContexttt = createContext();
+
+const initialData = { theme: localStorage.getItem("mytheme") === null  ? "Light"  : "Dark" };
+
+const reducer = (firstState, action) => {
+ switch (action.type) {
+   case "CHANGE_THEME":
+     return { ...firstState, theme: action.newValue };
+   default:
+     return firstState;
+ }}
+
+
+ 
+ export function ThemeProvider({ children }) {
+   const [firstState, dispatch] = useReducer(reducer, initialData);
+   const toggleTheme = (newName) => {
+    localStorage.setItem("mytheme", newName);
+     dispatch({ type: "CHANGE_THEME", newValue: newName });
+   };
+ 
+ 
+   return (
+      <ThemeContexttt.Provider value={{ ...firstState, toggleTheme }}>
+       {children}
+      </ThemeContexttt.Provider>
+   );
+ }
+ 
+ export default ThemeContexttt;
